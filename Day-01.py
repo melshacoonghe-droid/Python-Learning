5476d9b738d885065d7b903670036064efbf0de9
melshacoonghe-droid <melshacoonghe@gmail.com>
2026-06-02 20:41:25 +0530

    Update Day-01.py

--- Day-01.py
+++ Day-01.py
@@ -1,6 +1,6 @@
 
-mass = float(input("What is the mass?"))
-velocity = float(input("What is the velocity"))
-K = 0.5 * mass * (velocity**2)
+m = float(input("What is the mass?"))
+v = float(input("What is the velocity"))
+K = 0.5 * m * (v**2)
 
 print("Kinetic Energy is", K )
