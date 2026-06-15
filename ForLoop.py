09208e6d3fb80b0e677522faf853fc0e36bd60b5
melshacoonghe-droid <melshacoonghe@gmail.com>
2026-06-15 22:51:02 +0530

    ForLoop

--- ForLoop.py
+++ ForLoop.py
@@ -2,3 +2,24 @@ for i in range(4, 0, -1):
     for j in range(i, 0, -1):
         print(j, end="")
     print()
+
+    for i in range(1, 11):
+      if i == 6:
+        break
+    print(i)
+
+    for i in range(1, 11):
+      if i == 6:
+        continue
+    print(i)
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+    for i in range(1, 21):
+      if i % 7 == 0:
+        print("Found:", i)
+        break
+    print(i)
